19c293e9af8f71da29e88359b5fb5e9dac1292b9
TRosa Dev <thiagosrosa@gmail.com>
2023-11-25 09:35:09 -0300

    refactor: create readInput() and showMenu()

--- main.go
+++ main.go
@@ -1,36 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    // Print a message indicating the start of the application
-    fmt.Println("Starting application...")
+	// Print a message indicating the start of the application
+	fmt.Println("Starting application...")
 
-    // Display a menu for the user to choose an option
-    fmt.Println("Choose an option:")
-    fmt.Println("1 - Start monitoring")
-    fmt.Println("2 - Display logs")
-    fmt.Println("3 - Exit")
+	showMenu()
 
-    // Declare a variable to store the user's selected option
-    var selectedOption int
+	selectedOption := readInput()
 
-    // Prompt the user to input their choice and store it in the selectedOption variable
-    fmt.Scan(&selectedOption)
+	// Use a switch statement to perform actions based on the user's choice
+	switch selectedOption {
+	case 1:
+		fmt.Println("Monitoring....")
+	case 2:
+		fmt.Println("Displaying logs")
+	case 3:
+		fmt.Println("Bye! See you soon...")
+		os.Exit(0)
+		break
+	default:
+		fmt.Println("Selected option is invalid", selectedOption)
+	}
+}
 
-    // Print the selected option to the console
-    fmt.Println("Selected option: ", selectedOption)
+func readInput() int {
+	// Declare a variable to store the user's selected option
+	var selectedOption int
 
-    // Use a switch statement to perform actions based on the user's choice
-    switch selectedOption {
-    case 1:
-        fmt.Println("Monitoring....")
-    case 2:
-        fmt.Println("Displaying logs")
-    case 3:
-        // Exit the program for option 3
-        break
-    default:
-        fmt.Println("Selected option is invalid", selectedOption)
-    }
+	// Prompt the user to input their choice and store it in the selectedOption variable
+	fmt.Scan(&selectedOption)
+
+	// Print the selected option to the console
+	fmt.Println("Selected option: ", selectedOption)
+
+	return selectedOption
+}
+
+func showMenu() {
+	// Display a menu for the user to choose an option
+	fmt.Println("Choose an option:")
+	fmt.Println("1 - Start monitoring")
+	fmt.Println("2 - Display logs")
+	fmt.Println("3 - Exit")
 }
